27c4d6d88a24809e9108ba7d9c807fe4a09e39d5
python <857827469@qq.com>
2019-06-05 21:01:22 +0800

    集成配置类和集成flask_sqlalchemy

--- manage.py
+++ manage.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:127.0.0.1@3306/project_flask'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
