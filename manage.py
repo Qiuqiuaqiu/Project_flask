e839aecdc32313ae6adfd1bf562f7aa9976732b0
python <857827469@qq.com>
2019-06-06 15:00:48 +0800

    抽取配置类到指定的config中

--- manage.py
+++ manage.py
@@ -18,26 +18,7 @@ from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 
 
-class Config(object):
-    SECRET_KEY = '123131'
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:127.0.0.1@3306/project_flask'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-    # 给配置类里自定义两个属性
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-
-    # 指定session的存储方式
-    SESSION_TYPE = 'redis'
-    # 指定session的储存对象
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
-    # 设置session签名　加密
-    SESSION_USE_SIGNER = True
-    # 设置session永久保存
-    SESSION_PERMANENT = False
-    # 设置session保存时间
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
+from config import Config
 
 app = Flask(__name__)
 # 一、集成配置类
