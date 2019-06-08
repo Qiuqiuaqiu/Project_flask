055b7a9f0c53481013f44521365e025e6c3eabfa
python <857827469@qq.com>
2019-06-08 10:25:53 +0800

    添加静态文件,创建拖动templates/news/index; 渲染前台首页；创建favicon.ico视图函数并渲染

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,10 +1,16 @@
 from info.modules.index import index_blu
 
 from info import redis_store
-from flask import session
+from flask import session, render_template, redirect, current_app
+
 
 @index_blu.route('/')
 def index():
     redis_store.set('name','laowang')
     session["xiaohua"] = 'xiaohua'
-    return 'index'
+    return render_template("news/index.html")
+
+@index_blu.route('/favicon.ico')
+def favicon():
+    # return redirect("/static/news/favicon.ico")
+    return current_app.send_static_file("news/favicon.ico")
