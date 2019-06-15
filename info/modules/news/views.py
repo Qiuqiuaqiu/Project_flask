adbe8398fb9491532879bd9decf4e485dd1ef3ad
python <857827469@qq.com>
2019-06-15 21:05:53 +0800

    新闻详情页面渲染

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,5 +1,7 @@
-from flask import render_template, g
+from flask import render_template, g, abort, current_app
 
+from info import constants
+from info.models import News
 from info.modules.news import news_blu
 from info.utils.common import user_login
 
@@ -12,8 +14,40 @@ def detail(news_id):
     :param news_id:
     :return:
     """
+    # 1、查询点击排行新闻
+    clicks_news = []
+    try:
+        clicks_news = News.query.order_by(News.clicks.desc()).limit(
+            constants.CLICK_RANK_MAX_NEWS).all()  # [obj, obj, obj]
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # [obj, obj, obj] --> [{}, {}, {}, {}]
+    clicks_news_li = [news.to_basic_dict() for news in clicks_news]
+
+    # 2、显示新闻的具体信息
+    # 判断新闻id存不存在
+    if not news_id:
+        abort(404)
+    # 判断新闻id是不是整数类型
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        abort(404)
+    # 判断新闻是不是存在
+    news = None
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    if not news:
+        abort(404)
     user = g.user
     data = {
-        "user_info": user.to_dict() if user else None
+        "user_info": user.to_dict() if user else None,
+        "clicks_news_li": clicks_news_li,
+        "news": news.to_dict()
     }
     return render_template("news/detail.html",data=data)
