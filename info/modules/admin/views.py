792bb97341501172cad5aad34a5dc08c211c7f78
python <857827469@qq.com>
2019-06-16 22:45:12 +0800

    新闻版式编辑前后端实现

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -8,6 +8,48 @@ from info.modules.admin import admin_blu
 from info.utils.response_code import RET
 
 
+@admin_blu.route('/news_edit')
+def news_edit():
+    """新闻编辑"""
+    page = request.args.get("p", 1)
+    keywords = request.args.get("keywords", None)
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    news_list = []
+    current_page = 1
+    total_page = 1
+
+    filters = [News.status == 0]
+    # 如果关键字存在，那么就添加关键字搜索
+    if keywords:
+        filters.append(News.title.contains(keywords))
+    try:
+        paginate = News.query.filter(*filters) \
+            .order_by(News.create_time.desc()) \
+            .paginate(page, constants.ADMIN_NEWS_PAGE_MAX_COUNT, False)
+
+        news_list = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_dict_list = []
+    for news in news_list:
+        news_dict_list.append(news.to_basic_dict())
+
+    context = {"total_page": total_page,
+               "current_page": current_page,
+               "news_list": news_dict_list}
+
+    return render_template('admin/news_edit.html', data=context)
+
+
+
 @admin_blu.route('/news_review_action', methods=["POST"])
 def news_review_action():
     # 1. 接受参数
